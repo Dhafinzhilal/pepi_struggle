44661d8e51150a60d038cdcaccaf5aed641f9063
dhafinzhilal <dhafinzhilal91@gmail.com>
2024-02-26 10:00:46 +0700

    update

--- scene/level/hub.gd
+++ scene/level/hub.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var level_cam: Camera2D = get_node("CameraManipulation/Camera2D")
+@onready var playerchar: CharacterBody2D = get_node("player")
+
+
 func _ready():
 	pass
 
