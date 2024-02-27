afc98738074e6e1b2e741b0a8d72e9f1eeeb7533
dhafinzhilal <dhafinzhilal91@gmail.com>
2024-02-27 13:48:20 +0700

    0.4.4

--- editor/script/Editor_Object.gd
+++ editor/script/Editor_Object.gd
@@ -6,13 +6,13 @@ var is_panning = true
 @onready var level = get_node("/root/main/Level")
 @onready var editor = get_node("/root/main/cam_container")
 @onready var editor_cam = editor.get_node("Camera2D")
-
 @onready var tab_container = get_node("/root/main/item_select/Control/TabContainer")
-@onready var panel_layout = get_node("/root/main/item_select/Control/PanelLayoutTile/VBoxContainer/LayerOption")
-
+@onready var panel_layout = get_node("/root/main/item_select/Control/PanelLayoutTile/VBoxContainer")
+@onready var optionbutton1 = panel_layout.get_node("LayerOption")
 @onready var tile_map : TileMap = level.get_node("TileMap")
 
 var current_item
+var layertile_id :int 
 
 func _ready():
 	editor_cam.enabled = true
@@ -22,6 +22,7 @@ func _ready():
 
 func _process(delta):
 	global_position = get_global_mouse_position()
+	var layertile_focus = tile_map.get_layer_z_index(1)
 	
 	#this condition for placement any object
 	if !Global.place_tile:
@@ -32,13 +33,13 @@ func _process(delta):
 			new_item.global_position = get_global_mouse_position()
 		if can_place and Input.is_action_pressed("ctrl+left"):
 			pass
-	#if the object is tile
 	else:
 		if can_place and Input.is_action_pressed("mb_left"):
 			#this condition for placing tile into level
 			place_tile()
 		if can_place and Input.is_action_pressed("ctrl+left"):
 			del_tile()
+	
 	
 	move_editor(delta)
 	is_panning = Input.is_action_pressed("mb_middle")
@@ -47,18 +48,17 @@ func _process(delta):
 func place_tile():
 	global_position = get_global_mouse_position()
 	var mousepos = tile_map.local_to_map(global_position)
-	tile_map.set_cell(0, mousepos,Global.current_tile,Vector2i(0,0))
+	tile_map.set_cell(layertile_id, mousepos,Global.current_tile,Vector2i(0,0))
 	
 func del_tile():
 	global_position = get_global_mouse_position()
 	var mousepos = tile_map.local_to_map(global_position)
-	tile_map.set_cell(0, mousepos,-1,Vector2i(0,0))
+	tile_map.set_cell(layertile_id, mousepos,-1,Vector2i(0,0))
 
 func move_editor(delta):
 	var dir = Input.get_vector("a","d","w","s")
 	editor.global_position += dir * 1000 * delta
 	pass
-
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
--- editor/script/LayerOption.gd
+++ editor/script/LayerOption.gd
@@ -1,5 +1,5 @@
 extends OptionButton
-
+@onready var editor = get_node("/root/main/Editor_Object")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +9,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+func _on_LayerOption_item_selected(index):
+	editor.layertile_id = get_item_id(index)
+	pass # Replace with function body.
